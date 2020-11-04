48369c88015f4bec39ecb2fa98d6cc418d7abf42
peter_alfie <alfie1104@gmail.com>
2020-11-04 12:44:30 +0900

    Test Environment for end to end test is modified to have same env with real

--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -5,39 +5,39 @@ import { Movie } from './entities/movie.entity';
 
 @Injectable()
 export class MoviesService {
-    private movies : Movie[] = []; //가짜 DB에서 획득한 Movie 리스트
+  private movies: Movie[] = []; //가짜 DB에서 획득한 Movie 리스트
 
-    getAll(): Movie[]{
-        return this.movies
+  getAll(): Movie[] {
+    return this.movies;
+  }
+
+  getOne(id: number): Movie {
+    // 문자열 앞에 +를 붙임으로써 parseInt와 동일한 효과를 낼 수 있음
+    const movie = this.movies.find(movie => movie.id === id);
+    if (!movie) {
+      throw new NotFoundException(`Movie with ID ${id} not found.`); //NestJS가 제공하는 HttpException
     }
+    return movie;
+  }
 
-    getOne(id: number) : Movie{
-        // 문자열 앞에 +를 붙임으로써 parseInt와 동일한 효과를 낼 수 있음
-        const movie = this.movies.find(movie => movie.id === id)
-        if(!movie){
-            throw new NotFoundException(`Movie with ID ${id} not found.`)//NestJS가 제공하는 HttpException
-        }
-        return movie;
-    }
+  deleteOne(id: number) {
+    this.getOne(id);
+    this.movies = this.movies.filter(movie => movie.id !== id);
+  }
 
-    deleteOne(id : number) {
-        this.getOne(id);
-        this.movies = this.movies.filter(movie => movie.id !== id);
-    }
+  create(movieData: CreateMovieDto) {
+    this.movies.push({
+      id: this.movies.length + 1,
+      ...movieData,
+    });
 
-    create(movieData : CreateMovieDto){
-        this.movies.push({
-            id:this.movies.length + 1,
-            ...movieData
-        })
+    return true;
+  }
 
-        return true
-    }
+  update(id: number, updateData: UpdateMovieDto) {
+    const movie = this.getOne(id);
+    this.deleteOne(id);
 
-    update(id:number, updateData : UpdateMovieDto){
-        const movie = this.getOne(id);
-        this.deleteOne(id);
-
-        this.movies.push({...movie, ...updateData})
-    }
+    this.movies.push({ ...movie, ...updateData });
+  }
 }
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,17 +1,31 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { INestApplication } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
 import { AppModule } from './../src/app.module';
 
+/*
+  e2e(end to end) test는 어플리케이션과 관련된 모든 부분을 테스트할때 이용됨
+  실제 사용자가 웹사이트를 이용하는 것과 동일하게 테스트 가능
+*/
 describe('AppController (e2e)', () => {
   let app: INestApplication;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
     app = moduleFixture.createNestApplication();
+
+    //Testing 시에도 실제 운영처럼 동일한 환경을 적용해줘야한다.
+    //main.ts에서 useGlobalPipes를 이용하여 쿼리파라미터의 형변환을 자동으로 했으므로 test에서도 동일한 코드를 적용한다.
+    app.useGlobalPipes(
+      new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true,
+      }),
+    );
     await app.init();
   });
 
@@ -19,6 +33,47 @@ describe('AppController (e2e)', () => {
     return request(app.getHttpServer())
       .get('/')
       .expect(200)
-      .expect('Hello World!');
+      .expect('Welcome to my Movie API');
+  });
+
+  describe('/movies', () => {
+    it('GET', () => {
+      return request(app.getHttpServer())
+        .get('/movies')
+        .expect(200)
+        .expect([]);
+    });
+
+    it('POST', () => {
+      return request(app.getHttpServer())
+        .post('/movies')
+        .send({
+          title: 'Test',
+          year: 2000,
+          genres: ['test'],
+        })
+        .expect(201);
+    });
+
+    it('DELETE', () => {
+      return request(app.getHttpServer())
+        .delete('/movies')
+        .expect(404);
+    });
+  });
+
+  describe('/movies/:id', () => {
+    it('GET 200', () => {
+      return request(app.getHttpServer())
+        .get('/movies/1')
+        .expect(200);
+    });
+    it('GET 404', () => {
+      return request(app.getHttpServer())
+        .get('/movies/999')
+        .expect(404);
+    });
+    it.todo('DELETE');
+    it.todo('PATCH');
   });
 });
